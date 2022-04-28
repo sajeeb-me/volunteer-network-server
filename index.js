7762982edbf72e6f5704d00f8be939477fbc37b0
sdshuvo98 <hellosdshuvo@gmail.com>
2022-04-28 07:32:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    res.send('Home')
+})
+
+app.listen(port, () => {
+    console.log("Listening port: ", port)
+})
